fc07a8c75f4c9dd01b65362750f28505724dc093
Erick Devv <me440144@uaeh.edu.mx>
2023-10-15 16:43:36 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+import git from "simple-git";
+
+console.clear();
+
+console.log("\x1b[31m\x1b[1m----------------");
+
+console.log("| \x1b[0mCronos.js!\x1b[31m\x1b[1m🔥 |");
+console.log("----------------\x1b[0m");
+
+try {
+  await git().clone("https://github.com/cronos-js/cronos.express", ".");
+  console.log("🚀 Project created successfully!\n");
+} catch (error) {
+  console.log("😨 An error occurred while creating the project.\n");
+}
