9987caee6a90fb2ced5154a7f5b0b619fbe94c00
Erick Devv <111036821+ErickDevv@users.noreply.github.com>
2024-02-09 11:04:00 -0600

    Update types/Project.ts

--- types/Project.ts
+++ types/Project.ts
@@ -2,8 +2,6 @@ interface Project {
   name: string;
   value: string;
   type: 'local' | 'external';
-  body?: Body;
-  requestProjectName?: boolean;
   steps: Function[];
   path?: string;
   execCommand?: string | string[];
